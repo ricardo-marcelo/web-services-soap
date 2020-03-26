c906a7b06816c2f77904cc0c2c9c4962de67c944
Ricardo Marcelo <ricardomarcelo@hotmail.com>
2020-03-26 02:54:30 +0000

    JAX_WS Security Customer

---
+++ ClientesWS-SOAP-JAX-WS-Security-Consumer/src/main/java/pe/ricardomarcelo/ventas/ws/consumer/App.java
@@ -0,0 +1,54 @@
+package pe.ricardomarcelo.ventas.ws.consumer;
+
+import java.io.File;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+
+import pe.ricardomarcelo.ventas.ws.types.Cliente;
+import pe.ricardomarcelo.ventas.ws.types.ClienteWS;
+
+public class App 
+{
+		
+    public static void main( String[] args ) throws Exception
+    {
+    	
+		
+		Authenticator myAuth = new Authenticator() {
+			@Override 
+			protected PasswordAuthentication getPasswordAuthentication() {
+			  return new PasswordAuthentication("ricardo", "123456".toCharArray());
+			} 
+		};
+		
+		Authenticator.setDefault(myAuth);
+		 
+    	
+    	//File fileWSDL=new File("src/main/resources/ClienteWS.wsdl");        
+		//URL wsdlUrl = fileWSDL.toURI().toURL();
+    	URL wsdlUrl = new URL("http://localhost:8080/ClientesWS-SOAP-JAX-WS-Security/ClienteWS?wsdl");
+		QName qname = new QName("http://ricardomarcelo.pe/ws/ventas/","ClienteWS"); 
+		Service service = Service.create(wsdlUrl, qname); 
+		ClienteWS clienteWS = service.getPort(ClienteWS.class);
+		  
+		Cliente cliente = new Cliente(); cliente.setId(1);
+		cliente.setNombre("Ricardo"); cliente.setDireccion("Lima");
+		cliente.setTelefono("8887777");
+		 
+		/*
+		//Agregar user y pass al contenedor de autenticación 
+		BindingProvider bp = (BindingProvider) clienteWS;
+		  
+		bp.getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "ricardo");
+		bp.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "123456");
+		*/
+		String resulatdo = clienteWS.crear(cliente); System.out.println(resulatdo);
+		 			
+	 
+    }
+}
---
+++ ClientesWS-SOAP-JAX-WS-Security-Consumer/src/main/java/pe/ricardomarcelo/ventas/ws/types/ObjectFactory.java
@@ -0,0 +1,131 @@
+
+package pe.ricardomarcelo.ventas.ws.types;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the pe.ricardomarcelo.ventas.ws.types package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _ConsultarTodo_QNAME = new QName("http://ricardomarcelo.pe/ws/ventas/", "consultarTodo");
+    private final static QName _ConsultarTodoResponse_QNAME = new QName("http://ricardomarcelo.pe/ws/ventas/", "consultarTodoResponse");
+    private final static QName _Crear_QNAME = new QName("http://ricardomarcelo.pe/ws/ventas/", "crear");
+    private final static QName _CrearResponse_QNAME = new QName("http://ricardomarcelo.pe/ws/ventas/", "crearResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: pe.ricardomarcelo.ventas.ws.types
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link ConsultarTodo }
+     * 
+     */
+    public ConsultarTodo createConsultarTodo() {
+        return new ConsultarTodo();
+    }
+
+    /**
+     * Create an instance of {@link ConsultarTodoResponse }
+     * 
+     */
+    public ConsultarTodoResponse createConsultarTodoResponse() {
+        return new ConsultarTodoResponse();
+    }
+
+    /**
+     * Create an instance of {@link Crear }
+     * 
+     */
+    public Crear createCrear() {
+        return new Crear();
+    }
+
+    /**
+     * Create an instance of {@link CrearResponse }
+     * 
+     */
+    public CrearResponse createCrearResponse() {
+        return new CrearResponse();
+    }
+
+    /**
+     * Create an instance of {@link Cliente }
+     * 
+     */
+    public Cliente createCliente() {
+        return new Cliente();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ConsultarTodo }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link ConsultarTodo }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ricardomarcelo.pe/ws/ventas/", name = "consultarTodo")
+    public JAXBElement<ConsultarTodo> createConsultarTodo(ConsultarTodo value) {
+        return new JAXBElement<ConsultarTodo>(_ConsultarTodo_QNAME, ConsultarTodo.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ConsultarTodoResponse }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link ConsultarTodoResponse }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ricardomarcelo.pe/ws/ventas/", name = "consultarTodoResponse")
+    public JAXBElement<ConsultarTodoResponse> createConsultarTodoResponse(ConsultarTodoResponse value) {
+        return new JAXBElement<ConsultarTodoResponse>(_ConsultarTodoResponse_QNAME, ConsultarTodoResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Crear }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link Crear }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ricardomarcelo.pe/ws/ventas/", name = "crear")
+    public JAXBElement<Crear> createCrear(Crear value) {
+        return new JAXBElement<Crear>(_Crear_QNAME, Crear.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CrearResponse }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link CrearResponse }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://ricardomarcelo.pe/ws/ventas/", name = "crearResponse")
+    public JAXBElement<CrearResponse> createCrearResponse(CrearResponse value) {
+        return new JAXBElement<CrearResponse>(_CrearResponse_QNAME, CrearResponse.class, null, value);
+    }
+
+}
